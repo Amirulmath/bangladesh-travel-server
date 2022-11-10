2764bfd95c2b28cbe91ba062c7ecbe31fbf75f31
Amirulmath <amirulmath760@gmail.com>
2022-11-10 17:10:19 +0600

    Service detail API create

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,11 +10,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const service = require('./data/service.json');
 
-app.get('/service', (req, res) =>{
-    res.send(service);
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xjhllfd.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('bdTourist').collection('places');
+
+        app.get('/places', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const places = await cursor.toArray();
+            res.send(places);
+        });
+
+        app.get('/places/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);   
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Bangladesh Travel server is running')
